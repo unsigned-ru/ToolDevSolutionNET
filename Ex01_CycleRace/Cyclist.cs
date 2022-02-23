4a840c78f55d8674f9767bcb7db897b8c16bf499
Ruben Versavel <dev.ru.vero@gmail.com>
2022-02-23 14:40:03 +0100

    Add project files.

---
+++ Ex01_CycleRace/Cyclist.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_CycleRace
+{
+    public class Cyclist
+    {
+        private static readonly int MIN_SPEED = 1;
+        private static readonly int MAX_SPEED = 5;
+
+        public static readonly int MAX_CYCLIST = Enum.GetValues(typeof(ConsoleColor)).Length - 2;
+
+
+        public Cyclist(string name, ConsoleColor? color = null)
+        {
+            _rnd = new Random();
+            Name = name.ToUpper();
+
+            //generate random color if null
+            if (color == null)
+                RandomizeColor();
+            else
+                //set color
+                Color = color.GetValueOrDefault();
+        }
+
+        public string Name { get; set; }
+
+        public ConsoleColor Color { get; set; }
+
+        private uint _wheelState;
+        public uint WheelState {
+            get => _wheelState;
+
+            set
+            {
+                if (value >= _wheelchars.Length)
+                    value = 0;
+
+                _wheelState = value;
+            }
+        }
+
+
+        private int XPos { get; set; }
+
+        private readonly Random _rnd;
+
+        private readonly char[] _wheelchars = new[] { '/', '-', '\\', '|' };
+
+
+
+        public void Display()
+        {
+            Console.ForegroundColor = Color;
+
+
+            //create whitespace string
+            var wsString = new string(' ', XPos);
+
+            //print the bike with the correct wheels
+            Console.WriteLine(wsString + "   __°");
+            Console.WriteLine(wsString + " _\\<,_");
+            Console.WriteLine(wsString + $"({_wheelchars[WheelState]})/ ({_wheelchars[WheelState]})");
+
+            DisplayStreet();
+        }
+
+        private void DisplayStreet()
+        {
+            var street = Name.PadRight(Console.BufferWidth - 1, '=') + "|";
+            Console.WriteLine(street);
+        }
+
+        public bool Update()
+        {
+            WheelState++;
+            XPos += Math.Clamp(_rnd.Next(MIN_SPEED, MAX_SPEED), 0, Console.BufferWidth - 9);
+
+            return XPos >= Console.BufferWidth - 9;
+        }
+
+        public void RandomizeColor()
+        {
+            ConsoleColor color;
+
+            //generate random color that's not white or black
+            do { color = Program.GenerateRandomColor(_rnd); }
+            while (color == ConsoleColor.Black || color == ConsoleColor.White);
+
+            //assign it
+            Color = color;
+        }
+    }
+}
---
+++ Ex01_CycleRace/Program.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Ex01_CycleRace
+{
+    internal class Program
+    {
+
+        static T RequestInput<T>(string request, bool writeLine = false)
+        {
+            while (true)
+            {
+                if (writeLine) 
+                    Console.WriteLine(request);
+                else 
+                    Console.Write(request);
+
+                var input = Console.ReadLine();
+                if (input == null) 
+                    continue;
+
+                //check if we can convert input to specified type
+                var rv = Convert.ChangeType(input, typeof(T));
+                if (rv == null) 
+                    continue;
+
+                return (T) rv;
+            }
+        }
+
+        static T[] RequestInputArray<T>(string request, bool writeLine = false, char delim = ',', bool skipInvallid = true)
+        {
+            while (true)
+            {
+                start:
+                if (writeLine)
+                    Console.WriteLine(request);
+                else
+                    Console.Write(request);
+
+                var input = Console.ReadLine();
+                if (input == null) goto start;
+
+
+                var inputArr = input.Split(delim);
+
+                List<T> output = new List<T>();
+
+                foreach (var s in inputArr)
+                {
+                    //check if we can convert input to specified type
+                    var convertedObj = Convert.ChangeType(s, typeof(T));
+
+                    if (convertedObj == null)
+                        if (skipInvallid)
+                            continue;
+                        else 
+                            goto start;
+
+                    output.Add((T)convertedObj);
+                }
+
+                return output.ToArray();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /* Initialization */
+            Console.BufferWidth = Console.WindowWidth;
+
+            /* Console loop */
+            while (true)
+            {
+                /* input */
+                var names = RequestInputArray<string>("Enter name of the cyclist (seperated by ','): ");
+
+                if (names.Length > Cyclist.MAX_CYCLIST)
+                {
+                    Console.WriteLine($"Too many cyclists, you can only add up to: {Cyclist.MAX_CYCLIST}");
+                    continue;
+                }
+
+                Console.WriteLine($"Ready, {string.Join(", ", names.Take(names.Length-1))}{(names.Length > 1 ? $" and {names.Last()}" : "")}? Let the race begin!!");
+
+                //create cyclists
+                Cyclist[] cyclists = names.Select(s => new Cyclist(s.Trim())).ToArray();
+
+                //store cursor position
+                var (left, top) = Console.GetCursorPosition();
+
+                bool won = false;
+                Cyclist? winner = null;
+                while (!won)
+                {
+                    //set cursor at start to overwrite
+                    Console.SetCursorPosition(left, top);
+
+                    foreach (var cyclist in cyclists)
+                    {
+                        if (!won && cyclist.Update())
+                        {
+                            won = true;
+                            winner = cyclist;
+                        }
+
+                        cyclist.Display();
+                    }
+
+                    Thread.Sleep(100);
+                }
+
+                Debug.Assert(winner != null, nameof(winner) + " != null");
+                AnnounceWinner(winner);
+            }
+        }
+
+        private static void AnnounceWinner(Cyclist winner)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = winner.Color;
+            const char boxChar = '*';
+            
+            var winnerString = $"{boxChar} AND THE WINNER IS.... {winner.Name}!!! {boxChar}";
+            var boxBotTop = new string(boxChar, winnerString.Length);
+
+            Console.WriteLine(boxBotTop);
+            Console.WriteLine(winnerString);
+            Console.WriteLine(boxBotTop);
+        }
+
+        public static ConsoleColor GenerateRandomColor(Random? rand)
+        {
+            rand ??= new Random();
+
+            var consoleColorValues = Enum.GetValues(typeof(ConsoleColor));
+            var randNr = rand.Next(consoleColorValues.Length);
+            return (ConsoleColor)(consoleColorValues.GetValue(randNr) ?? throw new Exception("Random color returned null value"));
+        }
+    }
+}
